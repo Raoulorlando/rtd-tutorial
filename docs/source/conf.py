48fb35cec8108b75f81b888f5a20665d359ab6e2
Raoulorlando <32331954+Raoulorlando@users.noreply.github.com>
2024-02-12 11:02:58 +0100

    Update conf.py
    Test changes to the conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Pizza'
+copyright = '2023, Graziella'
+author = 'Cook'
 
 release = '0.1'
 version = '0.1.0'
